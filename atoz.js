1d7af5664db12bc7c059372421b98bbacb52731b
Douglas Martinez <douggmartinez12@gmail.com>
2023-10-11 22:29:51 -0500

    clean up code

--- atoz.js
+++ atoz.js
@@ -1,12 +1,12 @@
 let currentLetter = 'A';
 
-    // Cache DOM elements
+    
     const letterDisplay = document.getElementById('letter-display');
     const incrementButton = document.getElementById('increment-button');
     const decrementButton = document.getElementById('decrement-button');
     const letterInput = document.getElementById('letter-input');
 
-    // Event listeners
+    
     incrementButton.addEventListener('click', () => {
       incrementLetter();
     });
@@ -15,7 +15,6 @@ let currentLetter = 'A';
       decrementLetter();
     });
 
-    // Event listeners for button hover effect
     incrementButton.addEventListener('mouseover', () => {
       incrementButton.style.backgroundColor = '#fff';
       incrementButton.style.color = '#3498db';
@@ -36,7 +35,7 @@ let currentLetter = 'A';
       decrementButton.style.color = '#fff';
     });
 
-    // Function to increment the letter
+   
     function incrementLetter() {
       if (currentLetter < 'Z') {
         currentLetter = String.fromCharCode(currentLetter.charCodeAt(0) + 1);
@@ -44,7 +43,7 @@ let currentLetter = 'A';
       }
     }
 
-    // Function to decrement the letter
+    
     function decrementLetter() {
       if (currentLetter > 'A') {
         currentLetter = String.fromCharCode(currentLetter.charCodeAt(0) - 1);
@@ -52,7 +51,7 @@ let currentLetter = 'A';
       }
     }
 
-    // Helper function to update letter display
+    
     function updateLetterDisplay() {
       letterDisplay.textContent = currentLetter;
       letterInput.textContent = currentLetter;
